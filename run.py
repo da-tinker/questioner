0409799e6d6982d1be3b4b80213eba87fc9f1795
da-tinker <45802848+da-tinker@users.noreply.github.com>
2019-01-10 17:47:09 +0300

    Update run.py
    Update source env

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ import os
 from app import create_api_server
 
 config_name = os.getenv('APP_SETTINGS') # config_name = "development"
-api_server = create_api_server("development")
+api_server = create_api_server(config_name)
 
 if __name__ == '__main__':
     api_server.run()
